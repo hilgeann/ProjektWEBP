9909d601e72fd46dec46d7145e957a6596f57b9a
radejev <92913059+radejev@users.noreply.github.com>
2021-11-26 09:51:32 +0100

    arrays erstellt

---
+++ Applikation/app.js
@@ -0,0 +1,59 @@
+const F01 = {F01A:T05, F01B:F02, F01C:F03}
+const F02 = {F02A:F03, F02B:F05}
+const F03 = {F03A:F05, F03B:F07, F03C:F04, FO3D:F06}
+const F04 = {F04A:F08, F04B:F07}
+const F05 = {F05A:F07, F05B:F09}
+const F06 = {F06A:F08, F06B:F07}
+const F07 = {F07A:T03, F07B:F13}
+const F08 = {F08A:F07, F08B:F10}
+const F09 = {F09A:T06, F09B:T07, F09C:F12}
+const F10 = {F10A:T02, F10B:F11}
+const F11 = {F11A:T02, F11B:T01}
+const F12 = {F12A:T01, F12B:F13}
+const F13 = {F13A:T04, F13B:T06}
+
+const quizstrings = {
+    FO1:"ABC",
+    F02:"ABC",
+    F03:"ABC",
+    F04:"ABC",
+    F05:"ABC",
+    F06:"ABC",
+    F07:"ABC",
+    F08:"ABC",
+    F09:"ABC",
+    F10:"ABC",
+    F11:"ABC",
+    F12:"ABC",
+    F13:"ABC",
+    F01A:"ABC", 
+    F01B:"ABC", 
+    F01C:"ABC",
+    F02A:"ABC", 
+    F02B:"ABC", 
+    F03A:"ABC", 
+    F03B:"ABC", 
+    F03C:"ABC", 
+    FO3D:"ABC", 
+    F04A:"ABC", 
+    F04B:"ABC",
+    F05A:"ABC", 
+    F05B:"ABC",
+    F06A:"ABC", 
+    F06B:"ABC",
+    F07A:"ABC", 
+    F07B:"ABC",
+    F08A:"ABC", 
+    F08B:"ABC",
+    F09A:"ABC", 
+    F09B:"ABC", 
+    F09C:"ABC",
+    F10A:"ABC", 
+    F10B:"ABC",
+    F11A:"ABC", 
+    F11B:"ABC",
+    F12A:"ABC", 
+    F12B:"ABC",
+    F13A:"ABC", 
+    F13B:"ABC"
+    }
