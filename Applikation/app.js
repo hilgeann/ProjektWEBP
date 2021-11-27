41c05b40730fb9192416af1fb08b4909ae6cd0db
radejev <92913059+radejev@users.noreply.github.com>
2021-11-27 12:24:16 +0100

    diverse anpassungen

--- Applikation/app.js
+++ Applikation/app.js
@@ -86,3 +86,22 @@ function loadquestion(i) {
 function loadresult(i) {
     document.write(results.i)
 }
+
+
+function exp(){
+    const fs = require('fs');
+    let student = { 
+    name: 'Mike',
+    age: 23, 
+    gender: 'Male',
+    department: 'English',
+    car: 'Honda' };
+    let data = JSON.stringify(student, null, 2);
+    fs.writeFile(
+        'student-3.json', data, (err) => 
+            {if (err) throw err;
+            console.log('Data written to file');
+            }
+        )
+} 
+
