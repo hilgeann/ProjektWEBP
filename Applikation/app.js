8842b37b938629b791ea9fe332e2cd627e065ca4
radejev <92913059+radejev@users.noreply.github.com>
2021-11-30 20:31:53 +0100

    anpassungen 30.11

--- Applikation/app.js
+++ Applikation/app.js
@@ -33,7 +33,7 @@ const options = [{a:"Platzhalter"},
 
 const optnum = [0,3,2,4,2,2,2,2,2,3,2,2,2,2]
 
-const redirections = [{a:"Platzhalter"},
+const redirection = [{a:"Platzhalter"},
 {a:25, b:2, c:3},
 {a:3, b:5},
 {a:5, b:7, c:4, d:6},
@@ -60,28 +60,57 @@ const results = {
 }
 
 
-function loadresult(i) {
-document.write(results.i)
-}
+function choicea(i) {
+    let j = redirection[i].a;
+    loadquestion(j)
+};
+
+function choiceb(i) {
+    let j = redirection[i].b;
+    loadquestion(j)
+};
+
+function choicec(i) {
+    let j = redirection[i].c;
+    loadquestion(j)
+};
+
+function choiced(i) {
+    let j = redirection[i].d;
+    loadquestion(j)
+};
+    
+function start(){
+    loadquestion(1)
+};
 
 function loadquestion(i) {
-let j = optnum[i]
-document.getElementById("question").innerHTML = knots[i]
-if (j == 2) {
-    document.getElementById("opta").innerHTML = options[i].a;
-    document.getElementById("optb").innerHTML = options[i].b
-    document.getElementById("linec").innerHTML = "";
-    document.getElementById("lined").innerHTML = ""}
-else if (j == 3) {
-    document.getElementById("opta").innerHTML = options[i].a;
-    document.getElementById("optb").innerHTML = options[i].b;
-    document.getElementById("optc").innerHTML = options[i].c;
-    document.getElementById("lined").innerHTML = ""}
-else if (j == 4) {
-    document.getElementById("opta").innerHTML = options[i].a;
-    document.getElementById("optb").innerHTML = options[i].b;
-    document.getElementById("optc").innerHTML = options[i].c;
-    document.getElementById("optd").innerHTML = options[i].d}
+    let k = optnum[i]
+    document.getElementById("question").innerHTML = knots[i]
+    if (k == 2) {
+        document.getElementById("opta").innerHTML = options[i].a;
+        document.getElementById("opta").addEventListener("click", function() {choicea(i)});
+        document.getElementById("optb").innerHTML = options[i].b;
+        document.getElementById("optb").addEventListener("click", function() {choiceb(i)});
+        document.getElementById("linec").innerHTML = "";
+        document.getElementById("lined").innerHTML = ""}
+    else if (k == 3) {
+        document.getElementById("opta").innerHTML = options[i].a;
+        document.getElementById("opta").addEventListener("click", function() {choicea(i)});
+        document.getElementById("optb").innerHTML = options[i].b;
+        document.getElementById("optb").addEventListener("click", function() {choiceb(i)});
+        document.getElementById("optc").innerHTML = options[i].c;
+        document.getElementById("optc").addEventListener("click", function() {choicec(i)});
+        document.getElementById("lined").innerHTML = ""}
+    else if (k == 4) {
+        document.getElementById("opta").innerHTML = options[i].a;
+        document.getElementById("opta").addEventListener("click", function() {choicea(i)});
+        document.getElementById("optb").innerHTML = options[i].b;
+        document.getElementById("optb").addEventListener("click", function() {choiceb(i)});
+        document.getElementById("optc").innerHTML = options[i].c;
+        document.getElementById("optc").addEventListener("click", function() {choicec(i)});
+        document.getElementById("optd").innerHTML = options[i].d;
+        document.getElementById("optd").addEventListener("click", function() {choiced(i)});}
 }
 
-test
+start()
